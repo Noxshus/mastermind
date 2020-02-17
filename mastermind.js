d32f2cd47df367c6c8d6c76a1116d10834fbec7e
Noxshus <matthew@agius-muscat.net>
2020-02-17 15:35:54 +0100

    Added Solution basis code
    Minor

--- mastermind.js
+++ mastermind.js
@@ -1,17 +1,22 @@
 var data = {
-    playerguess: [0, 0, 0],
+    guess: [0, 0, 0],
     solution: [0, 0, 0],
     solutionlength: 3, //number of integers in the solution
     solutionceiling: 3, //the max value a solution integer can have
     solutionfloor: 0 //the min value a solution integer can have
 };
 
-function guess(num)
-{
+window.onload = function() {
+    for (i = 0; i < this.data.solution.length; i++) {
+        this.data.solution[i] = this.returnRandomInteger(data.solutionfloor, data.solutionceiling)
+        this.document.getElementById("sol" + i).innerHTML = this.data.solution[i];
+    }
+}
+
+function guess(num) {
     document.getElementById(num).innerHTML = returnRandomInteger(data.solutionfloor, data.solutionceiling);
 }
 
-function returnRandomInteger(min, max) //returns a random integer, min & max included
-{
+function returnRandomInteger(min, max) { //returns a random integer, min & max included
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
