2134d7ac54dfec87239984cb4479ba2df06c1b05
Noxshus <matthew@agius-muscat.net>
2020-03-03 16:30:48 +0100

    Initial work on increasing solution length

--- mastermind.js
+++ mastermind.js
@@ -5,15 +5,25 @@ var data = {
     solutionlength: 3, //this is technically not required, but will save having to constantly run .length
     solutionceiling: 3, //the max value a solution integer can have
     solutionfloor: 0, //the min value a solution integer can have
-    correctguess: 0, //currency
     errorguess: 0,
     solutionsolved: 0
 };
 
-window.onload = function() { //load some SOLUTION numbers at page load
-    for (i = 0; i < this.data.solutionlength; i++) {
+window.onload = function() {
+    for (i = 0; i < this.data.solutionlength; i++) { //load some SOLUTION numbers at page load
         this.data.solution[i] = this.returnRandomInteger(data.solutionfloor, data.solutionceiling);
         this.document.getElementById("sol" + i).innerHTML = this.data.solution[i];
+    }
+
+    document.getElementById("guessbuttondisable").disabled = true; //sometimes, on refresh, disabled button isn't disabled by default for some reason
+}
+
+var updateLoop = setInterval(update, 1000); //seperate to everything else, we run checks to see if stuff needs to be unlocked. Seperated from the guessLoop so that we don't spam checks needlessly
+
+function update() {
+    if (data.solutionsolved >= 10) //reveal solution length button after solving 10 codes
+    {
+        document.getElementById("solutionlengthbutton").style.display = "inline";
     }
 }
 
@@ -46,13 +56,11 @@ function compare() { //compare the solution & guess arrays, lock any matches
 
     for (i = 0; i < data.solutionlength; i++) {
         if (data.guess[i] == data.solution[i]) {
-            data.correctguess++;
             data.lock[i] = 1; //lock the number as solution is correct - will prevent generating more numbers until cleared
             _lockcount++; //if lockcount == data.solutionlength at the end of the method, it means all numbers have been solved
             document.getElementById("guess" + i).style.color = "#5cb85c";
         } 
-        else if (data.guess[i] != data.solution[i] && data.lock[i] == 0){
-            data.errorguess++;
+        else if (data.guess[i] != data.solution[i] && data.lock[i] == 0){           
             document.getElementById("guess" + i).style.color = "#d9534f";
         }
     }
@@ -60,11 +68,13 @@ function compare() { //compare the solution & guess arrays, lock any matches
     if (_lockcount == data.solutionlength) {
         data.solutionsolved++;
         generateSolution();
+    } 
+    else {
+        data.errorguess++;
     }
 
-    document.getElementById("correctguess").innerHTML = data.correctguess;
     document.getElementById("errorguess").innerHTML = data.errorguess;
-    document.getElementById("solutionsolved").innerHTML = data.solutionsolved;
+    document.getElementById("solvedsolution").innerHTML = data.solutionsolved;
 
 }
 
@@ -75,6 +85,30 @@ function generateSolution() { //used to create a new solution after solving the 
         document.getElementById("sol" + i).innerHTML = data.solution[i];
         document.getElementById("guess" + i).style.color = "#0275d8"; //some visual feedback that a solution was reached
     }
+}
+
+function upgradeSolutionLength()
+{
+    if (data.solutionsolved >= 10) {
+        data.solutionsolved = data.solutionsolved - 10;
+        data.solutionlength++;
+        data.solution.push(0);
+        data.guess.push(0);
+        data.lock.push(0);
+        var newSolutionElement = document.createElement("span");
+        newSolutionElement.id = "sol" + (data.solutionlength - 1);
+        newSolutionElement.innerHTML = 0;
+        document.getElementById("solutiondiv").appendChild(newSolutionElement);
+        var newGuessElement = document.createElement("span");
+        newGuessElement.id = "guess" + (data.solutionlength - 1);
+        newGuessElement.innerHTML = 0;
+        document.getElementById("guessdiv").appendChild(newGuessElement);
+    }    
+}
+
+function devAddSolution()
+{
+    data.solutionsolved = data.solutionsolved + 10;
 }
 
 function returnRandomInteger(min, max) { //returns a random integer, min & max included
